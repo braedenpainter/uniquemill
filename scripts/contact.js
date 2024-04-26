cc3a57938a475c45e4721b37c359c1f2edc6973f
Braeden Painter <braedenpainter1999@gmail.com>
2024-04-26 09:36:28 -0600

    Added a text area for the messages and styled them
    Got the email system working so now the page will send an email
    respective to the message type

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,4 @@
-function pullinfo() {
+/*function pullinfo() {
 	//Get Form elements
 	var custName = document.getElementById('custname').value;
 	var custEmail = document.getElementById('email').value;
@@ -12,4 +12,41 @@ function pullinfo() {
 	}
 	
 	//What to do with the information
+}*/
+
+function sendEmail() {
+	var messType;
+	const serviceID = 'service_tniwjb4';
+	var templateID;
+	
+	//Template review: template_rn75vuc
+	//Template jobrequest: template_cpo9yx2
+	
+	if(document.getElementById('review').checked) {
+		messType = document.getElementById('review').value;
+		templateID = 'template_rn75vuc';
+	} else {
+		messType = document.getElementById('jobrequest').value;
+		templateID = 'template_cpo9yx2';
+	}
+	
+	var params = {
+		name: document.getElementById('custname').value,
+		email: document.getElementById('email').value,
+		jobtype: document.getElementById('jobtype').value,
+		message: document.getElementById('message').value
+	};
+
+	emailjs.send(serviceID, templateID, params).then((res) => {
+		document.getElementById('custname').value = "";
+		document.getElementById('email').value = "";
+		document.getElementById('review').checked = false;
+		document.getElementById('jobrequest').checked = false;
+		document.getElementById('jobtype').selectedIndex = 0;
+		document.getElementById('message').value = "";
+
+		console.log(res);
+		alert('Message has been sent');
+	})
+	.catch((err) => console.log(err));
 }
